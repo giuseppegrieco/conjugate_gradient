c40b01f223f8aafe35ee24745ec043f951fadfd7
Mattia Sangermano <mattiasangermano1997@gmail.com>
2020-03-15 13:56:17 +0100

    - changed gitignore - changed create_data

--- create_data.m
+++ create_data.m
@@ -1,6 +1,37 @@
-function [] = create_data(arcs,values,iterations)
+function [] = create_data(arcs,d_values,iterations)
+%function [ ] = create_data( arcs, plot_title, iterations)
+%
+% Create all data needed for the experiments
+%
+%   [] = f(arcs, d_values, iterations )
+%
+% Input:
+%
+% - arcs is a [ 1 x n ] integers vector denoting the combinations of edges
+% used to generate different graphs.
+%
+% -  d_values is a [ 1 x n ] integers vector denoting the different
+% combinations of ranges in which to choose the diagonal values of the diagonal matrix.
+%
+% - iterations is a integer denoting the number of different example to
+% generate for each combination of edge and d_value
+
+% Output:
+%
+% For each combination of d_value and edge create a directory containg a number 
+% of different examples equal to the number of iterations chosen
+%
+%
+%{
+ =======================================
+ Author: Mattia Sangermano, Giuseppe Grieco
+ Date: 03-15-20
+ =======================================
+%}
+
+
 for arc = arcs
-    for d = values
+    for d = d_values
         directory_name = strcat('./data/edge_',num2str(arc),'_dval_',num2str(d));
         mkdir(directory_name)
         for i = 1:iterations
